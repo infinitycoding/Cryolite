2c2773ea9d859716cd8ae5f34ce947f2e54fd5ef
Peter Hösch <peter@peter-Satellite-L670D.(none)>
2013-05-17 20:38:35 +0200

    Better modelview.cpp

--- modelview.cpp
+++ modelview.cpp
@@ -2,15 +2,34 @@
 
 bool right=false;
 bool left=false;
-bool up=false;
-bool down=false;
 bool move_right=false;
 bool move_left=false;
-bool move_up=false;
-bool move_down=false;
 bool move_foreward=false;
 bool move_backward=false;
 
+float move_direction_foreward=0.01;
+float move_direction_left=0;
+
+float position_x = 0;
+float position_y = 0;
+float position_z = -3.5;
+
+
+void rotate_correctly(GLfloat angle, GLfloat rotate_x, GLfloat rotate_y, GLfloat rotate_z)
+{
+    glTranslatef(-position_x, -position_y, -position_z);
+    glRotatef(angle, rotate_x, rotate_y, rotate_z);
+    glTranslatef(position_x, position_y, position_z);
+}
+
+void move_camera(GLfloat move_x, GLfloat move_y, GLfloat move_z)
+{
+    position_x += move_x;
+    position_y += move_y;
+    position_z += move_z;
+
+    glTranslatef(move_x, move_y, move_z);
+}
 
 void process_events( void ){
     SDL_Event event;
@@ -35,41 +54,53 @@ void process_events( void ){
 }
 
 void rotation_handler(){    // Rotates the object if a key is pressed.
-    if(down)
-        glRotatef(0.1,1,0,0);
-    if(up)
-        glRotatef(-0.1,1,0,0);
     if(right)
-        glRotatef(0.1,0,1,0);
+    {
+        rotate_correctly(0.1,0,1,0);
+
+        if(move_direction_foreward <= 0)
+            move_direction_left += (float)1/90000;
+        else
+            move_direction_left -=  (float)1/90000;
+
+        if(move_direction_left <= 0)
+            move_direction_foreward -= (float)1/90000;
+        else
+            move_direction_foreward += (float)1/90000;
+
+    }
+
     if(left)
-        glRotatef(-0.1,0,1,0);
+    {
+        rotate_correctly(-0.1,0,1,0);
+
+        if(move_direction_foreward >= 0)
+            move_direction_left += (float)1/90000;
+        else
+            move_direction_left -=  (float)1/90000;
+
+        if(move_direction_left >= 0)
+            move_direction_foreward -= (float)1/90000;
+        else
+            move_direction_foreward += (float)1/90000;
+    }
 }
 
-void move_handler(){
-    if(move_up)
-        glTranslatef(0, -0.01, 0);
-    if(move_down)
-        glTranslatef(0, 0.01, 0);
+void move_handler(){        // Moves the object if a key is pressed
     if(move_right)
-        glTranslatef(-0.01,0,0);
+        move_camera(-move_direction_foreward,0,move_direction_left);
     if(move_left)
-        glTranslatef(0.01,0,0);
+        move_camera(move_direction_foreward,0,-move_direction_left);
     if(move_foreward)
-        glTranslatef(0,0,-0.01);
+        move_camera(move_direction_left,0,move_direction_foreward);
     if(move_backward)
-        glTranslatef(0,0,0.01);
+        move_camera(-move_direction_left,0,-move_direction_foreward);
 }
 
 void handle_key_down( SDL_keysym *keysym ){
     switch( keysym->sym ) {
         case SDLK_ESCAPE:   // Exit programm if Escape is pressed.
             exit(0);
-            break;
-        case SDLK_DOWN:     // Activate the rotation on keypress.
-            down=true;
-            break;
-        case SDLK_UP:
-            up=true;
             break;
         case SDLK_RIGHT:
             right=true;
@@ -78,22 +109,16 @@ void handle_key_down( SDL_keysym *keysym ){
             left=true;
             break;
         case SDLK_w:
-            move_up=true;
+            move_foreward=true;
             break;
         case SDLK_s:
-            move_down=true;
+            move_backward=true;
             break;
         case SDLK_a:
             move_left=true;
             break;
         case SDLK_d:
             move_right=true;
-            break;
-        case SDLK_1:
-            move_foreward=true;
-            break;
-        case SDLK_2:
-            move_backward=true;
             break;
         default:
             break;
@@ -104,12 +129,6 @@ void handle_key_down( SDL_keysym *keysym ){
 
 void handle_key_up( SDL_keysym *keysym ){
     switch( keysym->sym ) {
-        case SDLK_DOWN:     // Stop the rotation on keyrelease.
-            down=false;
-            break;
-        case SDLK_UP:
-            up=false;
-            break;
         case SDLK_RIGHT:
             right=false;
             break;
@@ -117,22 +136,16 @@ void handle_key_up( SDL_keysym *keysym ){
             left=false;
             break;
         case SDLK_w:
-            move_up=false;
+            move_foreward=false;
             break;
         case SDLK_s:
-            move_down=false;
+            move_backward=false;
             break;
         case SDLK_a:
             move_left=false;
             break;
         case SDLK_d:
             move_right=false;
-            break;
-        case SDLK_1:
-            move_foreward=false;
-            break;
-        case SDLK_2:
-            move_backward=false;
             break;
         default:
             break;
