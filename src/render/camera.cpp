a44ed3dadafde3279b9c0766aa1fed0fa6858f09
Peter Hösch <phoesch@gmx.de>
2013-06-11 18:22:27 +0200

    removed initilizing list warnings

--- src/render/camera.cpp
+++ src/render/camera.cpp
@@ -4,17 +4,26 @@
 
 Camera::Camera()
 {
-    position = {0, 0, 0};
+    position.x = 0;
+    position.y = 0;
+    position.z = 0;
 
-    looking_direction = {0, 0, 1};
+    looking_direction.x = 0;
+    looking_direction.y = 0;
+    looking_direction.z = 1;
 }
 
 
 Camera::Camera(GLfloat x_pos, GLfloat y_pos, GLfloat z_pos, GLfloat x_looking, GLfloat y_looking, GLfloat z_looking)
 {
-    position = {x_pos, y_pos, z_pos};
+    position.x = x_pos;
+    position.y = y_pos;
+    position.z = z_pos;
 
-    looking_direction = {x_looking, y_looking, z_looking};
+    looking_direction.x = x_looking;
+    looking_direction.y = y_looking;
+    looking_direction.z = z_looking;
+
     looking_direction = vunify(looking_direction);
 }
 
