6ac276ec8d620528839829bc824e32ec773cdaf1
tdotu <info@infinitycoding.de>
2013-09-01 23:22:03 +0200

    header update, luaproblem still unfixed

--- src/include/lua/script.h
+++ src/include/lua/script.h
@@ -35,16 +35,11 @@ class Script
 #define CBEGIN(NAME) int NAME(lua_State *L){
 #define CEND }
 
-#define NEWBEGIN(NAME) int NAME(lua_State *L){ luaL_checktype(L, 1, LUA_TTABLE); lua_newtable(L); lua_pushvalue(L,1); lua_setmetatable(L, -2); lua_pushvalue(L,1); lua_setfield(L, 1, "__index"); int NUMBEROFINSTANCES = -1;
-#define NEWEND(CLASS) luaL_getmetatable(L, #CLASS);lua_setmetatable(L, NUMBEROFINSTANCES); lua_setfield(L, NUMBEROFINSTANCES, "__self"); cout<<NUMBEROFINSTANCES<<endl; return 1;}
+#define NEWBEGIN(NAME) int NAME(lua_State *L){ luaL_checktype(L, 1, LUA_TTABLE); lua_newtable(L); lua_pushvalue(L,1); lua_setmetatable(L, -2); lua_pushvalue(L,1); lua_setfield(L, 1, "__index");
+#define NEWEND(CLASS) luaL_getmetatable(L, #CLASS);lua_setmetatable(L, -2); lua_setfield(L, -2, "__self"); return 1;}
 
 
-#define PRT(VALUE) lua_pushunsigned(L,VALUE); LUAPARANUM++;
-#define STR(VALUE) lua_pushstring(L,VALUE); LUAPARANUM++;
-#define DBL(VALUE) lua_pushnumber(L,VALUE); LUAPARANUM++;
-#define INT(VALUE) lua_pushinteger(L,VALUE); LUAPARANUM++;
-#define ALLOW_LCALL(...) int LUAPARANUM = 0
-#define LCALL(FUNCTION,...) lua_getglobal(L, #FUNCTION); LUAPARANUM = 0; __VA_ARGS__ lua_pcall(L,LUAPARANUM,1,0)
+#define addInstance(TYPE) (TYPE *)lua_newuserdata(L, sizeof(TYPE))
 
 static inline int INCLUASTACK(int *i)
 {
@@ -52,9 +47,15 @@ static inline int INCLUASTACK(int *i)
     return 0;
 }
 
-#define addInstance(TYPE) (TYPE *)lua_newuserdata(L, sizeof(TYPE)+INCLUASTACK(&NUMBEROFINSTANCES))
 #define getargc(...) lua_gettop(L)
 #define getInstance(TYPE, METATABLE, ...) GETINSTANCEFROMLUA<TYPE>(L, METATABLE, ##__VA_ARGS__)
+
+static inline int LPOP(lua_State *L, int n)
+{
+    lua_pop(L,n);
+    return 0;
+}
+
 
 template <typename T>
 static inline T GETINSTANCEFROMLUA(lua_State *L, const char *Metatable, int index = -1)
@@ -64,16 +65,31 @@ static inline T GETINSTANCEFROMLUA(lua_State *L, const char *Metatable, int inde
   return (T) luaL_checkudata(L, index, Metatable);
 }
 
-static inline void LRET(lua_State *L)
-{
-    lua_pop(L,1);
-}
+#define PRT(VALUE) lua_pushunsigned(L,VALUE); LUAPARANUM++;
+#define STR(VALUE) lua_pushstring(L,VALUE); LUAPARANUM++;
+#define DBL(VALUE) lua_pushnumber(L,VALUE); LUAPARANUM++;
+#define INT(VALUE) lua_pushinteger(L,VALUE); LUAPARANUM++;
 
+
+#define ALLOW_LCALL(...) int LUAPARANUM = 0
+#define LCALL(FUNCTION,...) lua_getglobal(L, #FUNCTION); LUAPARANUM = 0; __VA_ARGS__ lua_pcall(L,LUAPARANUM,1,0)
+
+#define LPRT
+#define LINT
+#define LDBL
+#define LSTR
+
+#define LRET(TYPE,...)
 
 
 template <typename T>
-static inline const T LRET(lua_State *L)
+static inline const T SLRET(lua_State *L, bool retv)
 {
+    if(!retv)
+    {
+        lua_pop(L,1);
+        return 0;
+    }
     T returnvalue;
     if(typeid(T) == typeid(char *) || typeid(T) == typeid(unsigned char *))
         returnvalue = (T)lua_tostring(L,-1);
@@ -87,6 +103,19 @@ static inline const T LRET(lua_State *L)
     lua_pop(L,1);
     return returnvalue;
 }
+
+
+static inline bool ISSET(const char *str = NULL)
+{
+    if(str)
+        return true;
+    return false;
+}
+
+
+
+
+
 
 
 #endif
