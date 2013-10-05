6b431223f09d4fa85a5729d239be9c192e803559
tdotu <info@infinitycoding.de>
2013-10-05 19:05:09 +0200

    added new macros

--- src/include/lua/script.h
+++ src/include/lua/script.h
@@ -31,32 +31,8 @@ class Script
         lua_State *lState;
 };
 
-// Just to keep ist unified
-#define CBEGIN(NAME) int NAME(lua_State *L){
-#define CEND }
 
-#define NEWBEGIN(NAME) int NAME(lua_State *L){
-#define CONSTRUCT(...) luaL_checktype(L, 1, LUA_TTABLE); lua_newtable(L); lua_pushvalue(L,1); lua_setmetatable(L, -2); lua_pushvalue(L,1); lua_setfield(L, 1, "__index")
-#define NEWEND(CLASS) luaL_getmetatable(L, #CLASS);lua_setmetatable(L, -2); lua_setfield(L, -2, "__self"); return 1;}
-
-
-#define addInstance(TYPE) (TYPE *)lua_newuserdata(L, sizeof(TYPE))
-
-static inline int INCLUASTACK(int *i)
-{
-    *i = *i-1;
-    return 0;
-}
-
-#define getargc(...) lua_gettop(L)
-#define getInstance(TYPE, METATABLE, ...) GETINSTANCEFROMLUA<TYPE>(L, METATABLE, ##__VA_ARGS__)
-
-static inline int LPOP(lua_State *L, int n)
-{
-    lua_pop(L,n);
-    return 0;
-}
-
+// DO NOT USE THE INLINE FUNCTIONS!!! USE THE MACROS!
 
 template <typename T>
 static inline T GETINSTANCEFROMLUA(lua_State *L, const char *Metatable, int index = -1)
@@ -66,52 +42,95 @@ static inline T GETINSTANCEFROMLUA(lua_State *L, const char *Metatable, int inde
   return (T) luaL_checkudata(L, index, Metatable);
 }
 
-#define PRT(VALUE) lua_pushunsigned(L,VALUE); LUAPARANUM++;
-#define STR(VALUE) lua_pushstring(L,VALUE); LUAPARANUM++;
-#define DBL(VALUE) lua_pushnumber(L,VALUE); LUAPARANUM++;
-#define INT(VALUE) lua_pushinteger(L,VALUE); LUAPARANUM++;
+
+static inline int LUA_INT(lua_State *L)
+{
+    int r = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    return r;
+}
 
 
-#define ALLOW_LCALL(...) int LUAPARANUM = 0
-#define LCALL(FUNCTION,...) lua_getglobal(L, #FUNCTION); LUAPARANUM = 0; __VA_ARGS__ lua_pcall(L,LUAPARANUM,1,0)
+static inline void LUA_INT(lua_State *L, int value)
+{
+    lua_pushinteger(L, value);
+}
 
-#define LPRT
-#define LINT
-#define LDBL
-#define LSTR
+static inline double LUA_NUM(lua_State *L)
+{
+    double r = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    return r;
+}
 
-#define LRET(TYPE,...)
+
+static inline void LUA_NUM(lua_State *L, double value)
+{
+    lua_pushnumber(L, value);
+}
+
+static inline const char *LUA_STR(lua_State *L)
+{
+    const char *str = lua_tostring(L, -1);
+    lua_pop(L, 1);
+    return str;
+}
+
+
+static inline void LUA_STR(lua_State *L, char *str)
+{
+    lua_pushstring(L, str);
+}
+
+template <typename T>
+static inline T LUA_DATA(lua_State *L, const char *Metatable)
+{
+    return GETINSTANCEFROMLUA<T>(L, Metatable);
+}
 
 
 template <typename T>
-static inline const T SLRET(lua_State *L, bool retv)
+static inline void LUA_DATA(lua_State *L, T value)
 {
-    if(!retv)
-    {
-        lua_pop(L,1);
-        return 0;
-    }
-    T returnvalue;
-    if(typeid(T) == typeid(char *) || typeid(T) == typeid(unsigned char *))
-        returnvalue = (T)lua_tostring(L,-1);
-    else if(typeid(T) == typeid(int) || typeid(T) == typeid(unsigned int) || typeid(T) == typeid(char) || typeid(T) == typeid(unsigned char) || typeid(T) == typeid(short) || typeid(T) ==typeid(unsigned short) )
-        returnvalue = (T)lua_tointeger(L,-1);
-    else if(typeid(T) == typeid(double) || typeid(T) == typeid(float))
-        returnvalue = (T)lua_tonumber(L,-1);
-    else
-        returnvalue = (T)lua_topointer(L,-1);
-
-    lua_pop(L,1);
-    return returnvalue;
+    T* udata = (T*) lua_newuserdata(L, sizeof(T));
+    *udata = value;
 }
 
 
-static inline bool ISSET(const char *str = NULL)
-{
-    if(str)
-        return true;
-    return false;
-}
+
+#define CBEGIN(NAME) int NAME(lua_State *L){
+#define CEND }
+
+#define NEWBEGIN(NAME) int NAME(lua_State *L){
+#define NEWEND(CLASS) luaL_getmetatable(L, #CLASS);lua_setmetatable(L, -2); lua_setfield(L, -2, "__self"); return 1;}
+
+#define CONSTRUCT(...) luaL_checktype(L, 1, LUA_TTABLE); lua_newtable(L); lua_pushvalue(L,1); lua_setmetatable(L, -2); lua_pushvalue(L,1); lua_setfield(L, 1, "__index")
+
+
+
+#define addInstance(TYPE) (TYPE *)lua_newuserdata(L, sizeof(TYPE))
+
+
+#define getargc(...) lua_gettop(L)
+#define getInstance(TYPE, METATABLE, ...) GETINSTANCEFROMLUA<TYPE>(L, METATABLE, ##__VA_ARGS__)
+
+
+#define ALLOW_LCALL(...) int LUAPARANUM = 0
+#define LCALL(FUNCTION, NRESULTS,...) lua_getglobal(L, #FUNCTION); LUAPARANUM = 0; __VA_ARGS__ lua_pcall(L,LUAPARANUM,NRESULTS,0)
+
+#define LINT(...) LUA_INT(L, ##__VA_ARGS__);
+#define LDAT(TYPE, TABLE_OR_VALUE) LUA_DATA<TYPE>(L, TABLE_OR_VALUE);
+#define LDBL(...) LUA_NUM(L, ##__VA_ARGS__);
+#define LSTR(...) LUA_STR(L, ##__VA_ARGS__);
+
+#define LCINT(VALUE)
+#define LCDAT(VALUE)
+#define LCDBL(VALUE)
+#define LCSTR(VALUE)
+
+
+
+
 
 
 
