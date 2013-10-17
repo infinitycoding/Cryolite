b34ab557fc60a7ec8596c9aa98c683f7b3942c78
tdotu <info@infinitycoding.de>
2013-10-17 17:58:12 +0200

    startet porting camera. Stopped because there is no way to check the metatable type

--- src/lua/cameraLW.cpp
+++ src/lua/cameraLW.cpp
@@ -1,34 +1,25 @@
 #include <lua/script.h>
 #include <camera.h>
-#include <iostream>
-
-#define CLASS camera
-
-using namespace std;
-/*
-
-NEWBEGIN(newcamera)
-    if(getargc() > 2)
-        return luaL_error(L,"invalid number of arguents %d",getargc());
-
-    double *x = addInstance(double);
-    *x = 20;
-NEWEND(CLASS)
 
 
-CBEGIN(getcamposition)
-    cout<<*((double *)luaL_checkudata(L, 0, "Camera"))<<endl;
-    //lua_pop(L,1);
-    //cout<<*getInstance(double*,"Camera")<<endl;
-    //cout<<*getInstance(double*,"Camera")<<endl;
-    return 0;
-CEND
+NEWBEGIN(newCamera)
+    switch(getargc())
+    {
+        case 1:
+            CONSTRUCT();
+            addInstance(Camera *, new Camera());
+        break;
+
+        case 2:
+        break;
+    };
+NEWEND(camera)
 
 
-luaL_Reg cameraTable[]
+
+
+reg cameraReg[]
 {
-    {"new", newcamera},
-    {"getPosition", getcamposition},
+    {"new", newCamera},
     {NULL, NULL}
 };
-*/
