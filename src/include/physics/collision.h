833786a076e592a39646c1acd8d4a40fc19429e9
Peter Hösch <phoesch@gmx.de>
2014-03-22 16:15:18 +0100

    fix ultimate bug of death

--- src/include/physics/collision.h
+++ src/include/physics/collision.h
@@ -33,12 +33,20 @@ class CollisionList
         bool collide(Object *obj1, Object *obj2);
         bool addCollision(Object *obj1, Object *obj2, vector *spot);
 
-        vector *boxBoxCollision(vector offset1, boundBox box1, vector offset2, boundBox box2);
-        vector *boxPlaneCollision(vector offset1, boundBox box, vector offset2, boundPlane plane);
-        vector *boxSphereCollision(vector offset1, boundBox box, vector offset2, boundSphere sphere);
-        vector *planePlaneCollision(vector offset1, boundPlane plane1, vector offset2, boundPlane plane2);
-        vector *planeSphereCollision(vector offset1, boundPlane plane, vector offset2, boundSphere sphere);
-        vector *sphereSphereCollision(vector offset1, boundSphere sphere1, vector offset2, boundSphere sphere2);
+        vector *boxBoxCollision(boundBox box1, boundBox box2);
+        vector *boxPlaneCollision(boundBox box, boundPlane plane);
+        vector *boxSphereCollision(boundBox box, boundSphere sphere);
+        vector *planePlaneCollision(boundPlane plane1, boundPlane plane2);
+        vector *planeSphereCollision(boundPlane plane, boundSphere sphere);
+        vector *sphereSphereCollision(boundSphere sphere1, boundSphere sphere2);
+
+        boundBox cloneBoundBox(boundBox *boxtemplate);
+        boundPlane cloneBoundPlane(boundPlane *planetemplate);
+        boundSphere cloneBoundSphere(boundSphere *spheretemplate);
+
+        boundBox preprocessBoundBox(boundBox *boxBefore, vector pos, vector scale, rotation rot);
+        boundPlane preprocessBoundPlane(boundPlane *planeBefore, vector pos, vector scale, rotation rot);
+        boundSphere preprocessBoundSphere(boundSphere *sphereBefore, vector pos, vector scale, rotation rot);
 
         List<Object> *objList;
         List<collision> *collList;
